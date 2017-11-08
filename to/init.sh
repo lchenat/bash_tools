f66661a798037b6422cb3dedc702cbf817d7d429
liychen <liychen@student.ethz.ch>
2017-11-08 15:49:22 -0800

    better tab completion for arguments

--- to/init.sh
+++ to/init.sh
@@ -8,31 +8,45 @@ function _complete_list() {
 	local cur
 	COMPREPLY=()
 	cur=${COMP_WORDS[COMP_CWORD]}
-	pre=${COMP_WORDS[COMP_CWORD-1]}
+	pre=${COMP_WORDS[${COMP_CWORD}-1]}
 	list=$( ls "$TO_DIR/list" )
 	IFS="/" read -r name path <<< "$cur"
 	if [[ "$cur" == */ ]]; then
 		path="$path/"
 	fi
-	if [[ "$cur" =~ / ]]; then
-		L=( $( compgen -W '$list' $name ) )
-		if [ ! -z "$L" ]; then	
-			p="$( cat "${TO_DIR}/list/$name" )/$path"
-			if [[ "$p" == */ ]]; then
-				p="${p}."
-			fi
-			dir=$( dirname "$p" )
-			base=$( basename "$p" )	
-			if [ $base = "." ]; then
-				base=""	
-			fi
-			COMPREPLY=( $( compgen -W "$( cd $dir && ls -1 -d -- */ 2>/dev/null )" -- $base ) )	
-			COMPREPLY=( "${COMPREPLY[@]/#/${cur%/*}/}" )
-		fi
+	if [[ "$cur" == -* ]]; then
+		COMPREPLY=( $( compgen -W "-c -d -l" -- $cur ) )
+		return 0
+	fi
+	if [[ "$pre" == -* ]]; then
+		case $pre in	
+			-d)
+				COMPREPLY=( $( compgen -W "$list" $cur ) )
+				;;
+			*)
+				;;
+		esac
 	else
-		COMPREPLY=( $( compgen -W "$list" $name ) )
-		if [[ ${#COMPREPLY[@]} -eq 1 ]]; then
-			COMPREPLY[0]="${COMPREPLY[0]}/"
+		if [[ "$cur" =~ / ]]; then
+			L=( $( compgen -W '$list' $name ) )
+			if [ ! -z "$L" ]; then	
+				p="$( cat "${TO_DIR}/list/$name" )/$path"
+				if [[ "$p" == */ ]]; then
+					p="${p}."
+				fi
+				dir=$( dirname "$p" )
+				base=$( basename "$p" )	
+				if [ $base = "." ]; then
+					base=""	
+				fi
+				COMPREPLY=( $( compgen -W "$( cd $dir && ls -1 -d -- */ 2>/dev/null )" -- $base ) )	
+				COMPREPLY=( "${COMPREPLY[@]/#/${cur%/*}/}" )
+			fi
+		else
+			COMPREPLY=( $( compgen -W "$list" $name ) )
+			if [[ ${#COMPREPLY[@]} -eq 1 ]]; then
+				COMPREPLY[0]="${COMPREPLY[0]}/"
+			fi
 		fi
 	fi
 	return 0
