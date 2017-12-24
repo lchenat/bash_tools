8c4d73eccf25556ffe88a967424fa8698804608e
liychen <liychen@student.ethz.ch>
2017-12-23 21:12:39 -0800

    git fast command

---
+++ git_cmd/init.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# get the script directory name
+# dirname: parse a path to get the directory name
+G_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd -P )"
+
+function g() {
+	case $1 in
+		push)
+			if [[ -z "$2" ]]; then
+				echo "Please write message"
+			else
+				git add .
+				git commit -m "$2"
+				git push origin master
+			fi
+			;;
+		fpush)
+			git push origin master --force
+			;;
+		fpull)
+			git fetch --all && git reset --hard origin/master
+			;;
+		*)
+			echo "undefined command"
+			;;
+	esac
+}
