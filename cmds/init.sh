26f2a19afaed139d548d3de38cec70299e62821c
liychen <liychen@student.ethz.ch>
2018-04-01 14:18:54 -0700

    cmds

--- cmds/init.sh
+++ cmds/init.sh
@@ -3,3 +3,8 @@
 function send_ssh_key() {
 	cat ~/.ssh/id_rsa.pub | ssh $1 "cat >> .ssh/authorized_keys"
 }
+
+# use this on other linux machine
+function send_git_ssh_key() {
+	scp ~/.ssh/id_rsa* $1:.ssh/
+}
