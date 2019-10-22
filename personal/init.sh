d154e3376b906973d2a76d7f05dc5340d70b17d7
liychen <liychen@student.ethz.ch>
2019-10-22 14:22:55 -0700

    fix borg command

--- personal/init.sh
+++ personal/init.sh
@@ -7,11 +7,7 @@ function mount-borg() {
     if [ ! -d $borg_dir ]; then
         mkdir $borg_dir
     fi
-    if [ "$(ls -A $borg_dir)" ]; then
-        echo 'Directory $borg_dir is not empty. Aborted.'
-    else
-        sshfs -o cache=yes -o kernel_cache -o compression=no liyuc@dash-borg.usc.edu:/media/borg2/liyuc/ $borg_dir # Use -o Ciphers=arcfour when supported
-    fi
+	sshfs -o cache=yes -o kernel_cache -o compression=no liyuc@dash-borg.usc.edu:/media/borg2/liyuc/ $borg_dir # Use -o Ciphers=arcfour when supported
 }
 
 function unmount-borg() {
@@ -21,8 +17,10 @@ function unmount-borg() {
 		return
     fi
     if [ $OSTYPE = "linux-gnu" ]; then
-        fusermount -u $borg_dir
+        #fusermount -u $borg_dir
+		umount -f $borg_dir
     elif [ $OSTYPE == "darwin"* ]; then
-        osxfuse -u $borg_dir
+        #osxfuse -u $borg_dir
+		umount -f $borg_dir
     fi
 }
